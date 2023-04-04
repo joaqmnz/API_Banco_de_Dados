6b4b3be1bdf5301b61efeeafd42a2bf5a1994593
Joaquim Luna <joaquimneto0000@gmail.com>
2023-04-03 21:00:17 -0300

    Testando swagger 2

--- api.php
+++ api.php
@@ -1,19 +1,5 @@
 <?php
 
-/**
- * @OA\Info(title="API Banco de Dados", version="1.0")
- */
-
-/**
- * @OA\Get(
- *     path="/api.php/usuarios",
- *     summary="Obtem todos os usuarios",
- *     tags={"Get"},
- *     @OA\Response(response="200", description="An example resource")
- * )
- */
-
-
 $_ENV["TOKEN"] = "Bearer joaquim1234";
 
 $api_message = [
--- documentation/api.php
+++ documentation/api.php
@@ -1,7 +1,7 @@
 <?php
 require("../vendor/autoload.php");
 
-$openapi = \OpenApi\Generator::scan([$_SERVER["DOCUMENT_ROOT"]."/api"]);
+$openapi = \OpenApi\Generator::scan([$_SERVER["DOCUMENT_ROOT"]."/api/models"]);
 
 header('Content-Type: application/json');
 echo $openapi->toJson();
